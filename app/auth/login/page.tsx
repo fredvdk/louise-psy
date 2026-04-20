ad31538bae457ceeee29afe3080e0e4711ee55d0
Frederick Van de Kerckhove <frederick.vandekerckhove@student.vives.be>
2026-04-20 18:46:27 -0400

    contd

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "@/components/navbar";
 export default function Page() {
   return (
     
-    <div className="container mx-auto">
+    <div className="w-full mx-auto">
       <Navbar showMenu={false}/>
       <div className="lg:w-1/3 md:w-1/2 mx-auto my-8">
         <LoginForm />
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { SignUpForm } from "@/components/sign-up-form";
 
 export default function Page() {
   return (
-    <div className="container mx-auto">
+    <div className="w-full mx-auto">
       <Navbar showMenu={false} />
       <div className="lg:w-1/3 md:w-1/2 mx-auto my-8">
         <SignUpForm />
